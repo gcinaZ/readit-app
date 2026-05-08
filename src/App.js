605ac1ef0a540c9082edf98da727ff411f65c2fb
gcinaZ <gcinazwane05@gmail.com>
2026-05-08 22:44:44 +0200

    Initial commit for Readit App

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Post from './Post';
+
+function App() {
+	return (
+		<div className="App">
+		<h1>ReadIt</h1>
+		<Post title="My fist post" body="Hello, this is my Reddit-style app!" />
+		</div>
+	);
+}
+export default App;
---
+++ src/Post.js
@@ -0,0 +1,10 @@
+function Post({ title, body }) {
+	return (
+		<div className="post">
+		<h2>{title}</h2>
+		<p>{body}</p>
+		</div>
+	);
+}
+
+export default Post;
